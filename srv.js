4523f58cd6fddbf2dac2c1bda428dda9a4fecee3
NextLiter <116687686+NextLiter@users.noreply.github.com>
2023-09-18 21:26:57 +0500

    Add files via upload

---
+++ srv.js
@@ -0,0 +1,60 @@
+import WebSocket, { WebSocketServer } from 'ws';
+import { randomUUID } from 'crypto';
+const clients = new Map(); // has to be a Map instead of {} due to non-string keys
+const wss = new WebSocketServer({ port: 8080 }); // initiate a new server that listens on port 8080
+
+// set up event handlers and do other things upon a client connecting to the server
+wss.on('connection', (ws) => {
+    // create an id to track the client
+    const id = randomUUID();
+    clients.set(ws, id);
+    console.log(`new connection assigned id: ${id}`);
+
+    serverBroadcastPlayerJoined(id);
+
+    // send a message to all connected clients upon receiving a message from one of the connected clients
+    ws.on('message', (data) => {
+        console.log(`received: ${data}`);
+        serverBroadcast(`Client ${clients.get(ws)} ${data}`);
+    });
+
+    // stop tracking the client upon that client closing the connection
+    ws.on('close', () => {
+        console.log(`connection (id = ${clients.get(ws)}) closed`);
+        clients.delete(ws);
+    });
+
+    // send the id back to the newly connected client
+    ws.send(`You have been assigned id ${id}`);
+});
+
+// send a message to all the connected clients about how many of them there are every 15 seconds
+setInterval(() => {
+    console.log(`Number of connected clients: ${clients.size}`);
+    serverBroadcast(`Number of connected clients: ${clients.size}`);
+}, 15000);
+
+// функция отправки сообщения каждому подключенному клиенту
+function serverBroadcast(message) {
+    wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(message);
+        }
+    });
+}
+
+
+function serverBroadcastPlayerJoined(clnt) {
+    wss.clients.forEach((client) => {
+        //client.send("JOINED ID TEST " + clnt)
+        //client.send("JOINED ID TEEE " +)
+        //Отправляется всем, кроме подключившемуся игроку
+        if (clients.get(client) != clnt) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send("JOINED "+clnt);
+            }
+        }
+    });
+}
+console.log('_________________________________________________');
+console.log('The server is running and waiting for connections');
